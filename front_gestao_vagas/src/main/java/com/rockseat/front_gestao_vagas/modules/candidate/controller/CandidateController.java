9b201926efaf2822b5dcd40c388fef64a8e528be
Carlos Henrique <ichfs9108@gmail.com>
2024-04-09 23:47:35 -0300

    feat: RestTemplate para acesso a API externa

--- front_gestao_vagas/src/main/java/com/rockseat/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ front_gestao_vagas/src/main/java/com/rockseat/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,15 +1,37 @@
 package com.rockseat.front_gestao_vagas.modules.candidate.controller;
 
+import com.rockseat.front_gestao_vagas.modules.candidate.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @GetMapping("/login")
+    @Autowired
+    private CandidateService candidateService;
+
+    @GetMapping("/login.html")
     public String login() {
-        return "candidate/login";
+        return "candidate/login.html";
+    }
+
+    @PostMapping("/signIn")
+    public String signIn(RedirectAttributes redirectAttributes, String username, String password) {
+
+        try {
+            var token = this.candidateService.login(username, password);
+
+            return "redirect:/candidate/profile.html";
+
+        } catch (HttpClientErrorException e) {
+            redirectAttributes.addFlashAttribute("error_message", "Usuário/Senha incorretos");
+            return "redirect:/candidate/login.html";
+        }
     }
 }
